f686a4f9f3378997e9d3e8af5867482c79559746
Luke <luke.wismer@hotmail.com>
2019-05-01 08:58:42 -0400

    Responder modded pt2

--- Responder.java
+++ Responder.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-
-
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -34,5 +32,21 @@ public class Responder
         int index = randomGenerator.nextInt(responses.size());
         return responses.get(index);
         //return "That sounds interesting. Tell me more...";
+    }
+
+    private void fillResponses()
+    {
+        responses.add("That sounds odd. Could you describe \n" +
+        "that problem in more detail?");
+        responses.add("No other customer has ever \n" +
+        "complained about this before \n" +
+        "What is your system configuration?");
+        responses.add("Thats a known problem with Vista." +
+        "Windows 7 is much better.");
+        responses.add("I need a bit more information on that");
+       
+        responses.add("have you checked that you do not /n" +
+        "have a dll conflict?");
+        
     }
 }
